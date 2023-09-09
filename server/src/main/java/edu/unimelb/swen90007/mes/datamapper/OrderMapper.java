06576e526271fdfee0aa170303995547ea805835
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-09-09 21:56:06 +1000

    add order mapper

--- server/src/main/java/edu/unimelb/swen90007/mes/datamapper/OrderMapper.java
+++ server/src/main/java/edu/unimelb/swen90007/mes/datamapper/OrderMapper.java
@@ -1,4 +1,27 @@
 package edu.unimelb.swen90007.mes.datamapper;
 
-public class OrderMapper {
+import edu.unimelb.swen90007.mes.model.Order;
+
+import java.util.List;
+
+public final class OrderMapper {
+    public static void create(Order order) {
+
+    }
+
+    public static List<Order> loadByCustomer(int customerID) {
+        return null;
+    }
+
+    public static List<Order> loadByEvent(int eventID) {
+        return null;
+    }
+
+    public static void updateStatus(int id, String newStatus) {
+
+    }
+
+    public static void delete(int id) {
+
+    }
 }
