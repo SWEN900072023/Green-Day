7dcea0bc9b4661fafdabaf3f391746c81e64cbb3
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-09-10 18:04:19 +1000

    supplement unit of work

--- server/src/main/java/edu/unimelb/swen90007/mes/util/UnitOfWork.java
+++ server/src/main/java/edu/unimelb/swen90007/mes/util/UnitOfWork.java
@@ -5,9 +5,14 @@ import java.util.List;
 
 public class UnitOfWork {
     private static final ThreadLocal<UnitOfWork> current = new ThreadLocal<>();
+    private static UnitOfWork instance;
     private final List<Object> newObjects = new ArrayList<>();
     private final List<Object> dirtyObjects = new ArrayList<>();
     private final List<Object> deletedObjects = new ArrayList<>();
+
+    private UnitOfWork() {
+
+    }
 
     public static void newCurrent() {
         setCurrent(new UnitOfWork());
@@ -21,16 +26,30 @@ public class UnitOfWork {
         current.set(uow);
     }
 
-    public void registerNew(Object obj) {
-
+    public static UnitOfWork getInstance() {
+        if (instance == null)
+            instance = new UnitOfWork();
+        return instance;
     }
 
-    public void registerDirty(Object obj) {
-
+    public void registerNew(Object o) {
+        if (newObjects.contains(o) || dirtyObjects.contains(o) || deletedObjects.contains(o))
+            return;
+        newObjects.add(o);
     }
 
-    public void registerDeleted(Object obj) {
+    public void registerDirty(Object o) {
+        if (newObjects.contains(o) || dirtyObjects.contains(o) || deletedObjects.contains(o))
+            return;
+        dirtyObjects.add(o);
+    }
 
+    public void registerDeleted(Object o) {
+        if (newObjects.remove(o))
+            return;
+        dirtyObjects.remove(o);
+        if (!deletedObjects.contains(o))
+            deletedObjects.add(o);
     }
 
     public void commit() {
