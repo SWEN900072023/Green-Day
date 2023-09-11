db9c6aa04f2255b7ee9ef364aa230110c873adf2
Benjamin Qian <qianjingning0903@gmail.com>
2023-09-11 23:44:53 +1000

    UI-Route/navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Signup from "./components/authentication/signup";
+import Signup from "./components/routes/authentication/signup";
+import Signin from "./components/routes/authentication/signin";
+import Navigation from "./components/routes/navigation/navigation";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Signup />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Signup />} />
+        <Route path="login" element={<Signin />}></Route>
+
         {/* <Route></Route> */}
       </Route>
     </Routes>
