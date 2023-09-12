3b4dd702ec11fd591926087917405e1ec8707336
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-09-12 20:00:40 +1000

    update unit of work

--- server/src/main/java/edu/unimelb/swen90007/mes/util/UnitOfWork.java
+++ server/src/main/java/edu/unimelb/swen90007/mes/util/UnitOfWork.java
@@ -3,12 +3,18 @@ package edu.unimelb.swen90007.mes.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The unit of work class.<br>
+ * We employ caller registration, i.e., callers are responsible for registering new/dirty objects with the unit of work. <br>
+ * Note that it does not maintain a list of deleted objects since no business transaction needs to delete records from the database. <br>
+ * After an event planner cancels the event, the system will update the event status to Cancelled in the table. <br>
+ * After a customer cancels the order, the system will update the order status to Cancelled in the table.
+ */
 public class UnitOfWork {
     private static final ThreadLocal<UnitOfWork> current = new ThreadLocal<>();
     private static UnitOfWork instance;
     private final List<Object> newObjects = new ArrayList<>();
     private final List<Object> dirtyObjects = new ArrayList<>();
-    private final List<Object> deletedObjects = new ArrayList<>();
 
     private UnitOfWork() {
 
@@ -25,23 +31,15 @@ public class UnitOfWork {
     }
 
     public void registerNew(Object o) {
-        if (newObjects.contains(o) || dirtyObjects.contains(o) || deletedObjects.contains(o))
+        if (newObjects.contains(o) || dirtyObjects.contains(o))
             return;
         newObjects.add(o);
     }
 
     public void registerDirty(Object o) {
-        if (newObjects.contains(o) || dirtyObjects.contains(o) || deletedObjects.contains(o))
+        if (newObjects.contains(o) || dirtyObjects.contains(o))
             return;
         dirtyObjects.add(o);
-    }
-
-    public void registerDeleted(Object o) {
-        if (newObjects.remove(o))
-            return;
-        dirtyObjects.remove(o);
-        if (!deletedObjects.contains(o))
-            deletedObjects.add(o);
     }
 
     public void commit() {
