7f5bd4abdd4c5bdf7af23e1df2cab01ac690ab8b
Benjamin Qian <qianjingning0903@gmail.com>
2023-09-18 02:59:10 +1000

    navigation-bar-formchange-based-on-userType

--- client/src/components/routes/navigation/navigation.jsx
+++ client/src/components/routes/navigation/navigation.jsx
@@ -1,35 +1,56 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 import "./navigation.style.scss";
+import { useSelector } from "react-redux";
+import { selectorCurrentUser } from "../../store/user/user.selector";
 const Navigation = () => {
+  const currentUser = useSelector(selectorCurrentUser);
   return (
     <Fragment>
       <div className="navigation">
         {/* TODO:When user login, we display home icon for user back to home page */}
         <h2>Welcome To Music Events System</h2>
         <div className="nav-links-container">
-          <Link className="nav-link" to={"/"}>
-            Sign up
-          </Link>
-          <Link className="nav-link" to={"/login"}>
-            Sign in
-          </Link>
-          <Link className="nav-link" to={"/"}>
-            Sign out
-          </Link>
-          {/* TODO: Check user role. If role is user, then display myBooking. If role is event planner, then display create event*/}
-          <Link className="nav-link" to={"/mybookings"}>
-            Bookings
-          </Link>
-          <Link className="nav-link" to={"/eventCreation"}>
-            Create Event
-          </Link>
-          <Link className="nav-link" to={"/User"}>
-            User Info
-          </Link>
-          <Link className="nav-link" to={"/venueCreation"}>
-            Create Venue
-          </Link>
+          {currentUser == null ? (
+            <>
+              <Link className="nav-link" to={"/"}>
+                Sign up
+              </Link>
+              <Link className="nav-link" to={"/login"}>
+                Sign in
+              </Link>
+            </>
+          ) : currentUser.userType === "EventPlanner" ? (
+            <>
+              <Link className="nav-link" to={"/"}>
+                Sign out
+              </Link>
+              <Link className="nav-link" to={"/eventCreation"}>
+                Create Event
+              </Link>
+            </>
+          ) : currentUser.userType === "Customer" ? (
+            <>
+              <Link className="nav-link" to={"/"}>
+                Sign out
+              </Link>
+              <Link className="nav-link" to={"/mybookings"}>
+                Bookings
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link className="nav-link" to={"/"}>
+                Sign out
+              </Link>
+              <Link className="nav-link" to={"/User"}>
+                User Info
+              </Link>
+              <Link className="nav-link" to={"/venueCreation"}>
+                Create Venue
+              </Link>
+            </>
+          )}
         </div>
       </div>
       <Outlet />
