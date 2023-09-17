9dc5ef22d683b95ce9d4775423f49c43fa72073d
StevenDB <xieyijie98@gmail.com>
2023-09-17 19:46:47 +1000

    feat[event-planner]: invite another planner

---
+++ server/src/main/java/edu/unimelb/swen90007/mes/servlet/PlannerInviteServlet.java
@@ -0,0 +1,59 @@
+package edu.unimelb.swen90007.mes.servlet;
+
+import edu.unimelb.swen90007.mes.constants.Constant;
+import edu.unimelb.swen90007.mes.exceptions.PermissionDeniedException;
+import edu.unimelb.swen90007.mes.model.Event;
+import edu.unimelb.swen90007.mes.model.EventPlanner;
+import edu.unimelb.swen90007.mes.service.IEventPlannerService;
+import edu.unimelb.swen90007.mes.service.impl.EventPlannerService;
+import edu.unimelb.swen90007.mes.util.JwtUtil;
+import edu.unimelb.swen90007.mes.util.ResponseWriter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet(name = "PlannerInviteServlet", urlPatterns = Constant.API_PREFIX + "/planner/invite/*")
+public class PlannerInviteServlet extends HttpServlet {
+
+    private static final Logger logger = LogManager.getLogger(PlannerInviteServlet.class);
+
+    /**
+     * POST
+     * [/planner/invite/{inviteeId}/{eventId}] invite another event
+     */
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String requestPath = request.getPathInfo();
+        String[] pathStrings = requestPath.split("/");
+        if (pathStrings.length != 3) {
+            ResponseWriter.write(response, 400, "Bad request");
+        }
+        String inviteeIdString = pathStrings[1];
+        String eventIdString = pathStrings[2];
+
+        try {
+            Integer inviterId = JwtUtil.getInstance().getUserId(request);
+            Integer inviteeId = Integer.parseInt(inviteeIdString);
+            Integer eventId = Integer.parseInt(eventIdString);
+            IEventPlannerService eventPlannerService = new EventPlannerService();
+            eventPlannerService.inviteEventPlanner(
+                    new EventPlanner(inviterId), new EventPlanner(inviteeId), new Event(eventId));
+            ResponseWriter.write(response, 200, "Success");
+
+        } catch (NumberFormatException e) {
+            ResponseWriter.write(response, 400, "Invalid path input");
+        } catch (PermissionDeniedException e) {
+            ResponseWriter.write(response, 403, e.getMessage());
+        } catch (SQLException e) {
+            ResponseWriter.write(response, 500, "Unexpected system error");
+            logger.error(e.getMessage());
+        }
+    }
+
+}
