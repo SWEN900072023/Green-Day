6cc5c9a7edb726e2fcb71de6dfae3aa50ffc7a9a
Benjamin Qian <qianjingning0903@gmail.com>
2023-09-19 13:13:47 +1000

    event-creation-integration-done

--- client/src/components/routes/eventCreation-planner/eventCreation.jsx
+++ client/src/components/routes/eventCreation-planner/eventCreation.jsx
@@ -1,8 +1,9 @@
 import "./eventCreation.scss";
 import Form from "../../form-input/form-input";
 import Button from "../../button/button";
-import { useState } from "react";
-import { createRoot } from "react-dom/client";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import { useEffect, useState } from "react";
 import Axiosapi from "../../axiosAPI/api";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
@@ -12,8 +13,16 @@ import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { MultiInputDateTimeRangeField } from "@mui/x-date-pickers-pro/MultiInputDateTimeRangeField";
+
+import { useSelector } from "react-redux";
+import { selectorCurrentUser } from "../../store/user/user.selector";
 
 const EventCreation = () => {
+  const currentUser = useSelector(selectorCurrentUser);
   const currencies = [
     {
       value: "USD",
@@ -35,83 +44,110 @@ const EventCreation = () => {
   const [accordions, setAccordions] = useState([]);
   const [title, setTitle] = useState("");
   const [artist, setArtist] = useState("");
-  const [startTime, setStartTime] = useState("");
-  const [endTime, setEndTime] = useState("");
-  const [sectionName, setSectionName] = useState("");
-  const [price, setPrice] = useState("");
-  const [capacity, setCapacity] = useState("");
-  //   const signUp = async (e) => {
-  //     e.preventDefault();
-  //     await Axiosapi.post("/register", {
-  //       email,
-  //       lastName,
-  //       firstName,
-  //       password,
-  //     }).then((res) => console.log(res));
-  //   };
+  const [timeRange, setTimeRange] = useState([]);
+  // const [startTime, setStartTime] = useState("");
+  // const [endTime, setEndTime] = useState("");
+  const [sections, setSections] = useState("");
+  const [venues, setVenues] = useState([{ name: "opera" }]);
+  const [venue, setVenue] = useState("");
+  // console.log(sections);
   const createNewElement = () => {
-    // const container = document.getElementById("accordion");
-    const accordionElement = (
-      <Accordion sx={{ marginTop: "1rem" }}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Accordion 1</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-    );
-    // (accordionElement, container);
-    setAccordions((prevAccordions) => [...prevAccordions, accordionElement]);
+    setAccordions((prevAccordions) => [
+      ...prevAccordions,
+      { name: "", currency: "AUD", unitPrice: "", capacity: "" },
+    ]);
   };
-  const createEvent = () => {};
+  useEffect(() => {
+    async function getVenues() {
+      await Axiosapi.get("/public/venues", {
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      }).then((res) => {
+        console.log(res.data.data);
+        setVenues(res.data.data);
+      });
+    }
+
+    getVenues();
+  }, []);
+  useEffect(() => {
+    setSections(accordions);
+  }, [accordions]);
+  const handleDateTimeRangeChange = (value) => {
+    // console.log("Received value:", context);
+    setTimeRange({
+      value,
+    });
+  };
+  const createEvent = async (e) => {
+    e.preventDefault();
+    // console.log(sections);
+    console.log(timeRange);
+    // console.log(Date(timeRange.value[0].$d));
+    const selectedVenue = venues.filter((v) => v.name === venue);
+    await Axiosapi.post(
+      "/planner/events",
+      {
+        title,
+        artist,
+        startTime: timeRange.value[0].$d,
+        endTime: timeRange.value[1].$d,
+        sections,
+        venueId: selectedVenue[0].id,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${currentUser.token}`,
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+      alert("event create successfully");
+    });
+  };
+  // console.log(venue);
   return (
     // <div className="container">
     <div className="Contain">
       <h1>im event creation</h1>
       <div className="event-creation-container">
         <div className="creation-container">
-          <form onSubmit={createEvent}>
-            <Form
-              label="Event Title"
-              type="text"
-              required
-              name="Title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-            ></Form>
-            <Form
-              label="Artist"
-              type="text"
-              required
-              onChange={(e) => setArtist(e.target.value)}
-              name="Artist"
-              value={artist}
-            ></Form>
-            <Form
-              label="Start Time"
-              type="date"
-              required
-              onChange={(e) => setStartTime(e.target.value)}
-              name="Start Time"
-              value={startTime}
-            ></Form>
-            <Form
-              label="End Time"
-              type="date"
-              required
-              onChange={(e) => setEndTime(e.target.value)}
-              name="End Time"
-              value={endTime}
-            ></Form>
-          </form>
+          <Form
+            style={{ marginBottom: "2px" }}
+            label="Event Title"
+            type="text"
+            required
+            name="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></Form>
+          <Form
+            // style={{ marginTop: "0.5rem" }}
+            label="Artist"
+            type="text"
+            required
+            onChange={(e) => setArtist(e.target.value)}
+            name="Artist"
+            value={artist}
+          ></Form>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer
+              components={[
+                "MultiInputDateTimeRangeField",
+                "SingleInputDateTimeRangeField",
+              ]}
+            >
+              <MultiInputDateTimeRangeField
+                slotProps={{
+                  textField: ({ position }) => ({
+                    label: position === "start" ? "StartTime" : "endTime",
+                  }),
+                }}
+                onChange={handleDateTimeRangeChange}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
         </div>
         <div className="section-creation-container">
           <div className="section-setting">
@@ -126,7 +162,7 @@ const EventCreation = () => {
               <AddIcon />
             </button>
           </div>
-          {accordions == [] ? (
+          {accordions.length === 0 ? (
             <></>
           ) : (
             accordions.map((accordion, index) => {
@@ -144,50 +180,96 @@ const EventCreation = () => {
                       label="Section Name"
                       type="text"
                       required
-                      onChange={(e) => setSectionName(e.target.value)}
+                      onChange={(e) => {
+                        const updatedAccordions = accordions.map(
+                          (accordion, accIndex) => {
+                            if (index === accIndex) {
+                              return { ...accordion, name: e.target.value };
+                            }
+                            return accordion;
+                          }
+                        );
+                        setAccordions(updatedAccordions);
+                      }}
                       name="Section Name"
-                      value={sectionName}
+                      value={accordions[index].name}
                     ></Form>
                     <Form
                       label="Price"
                       type="number"
                       required
-                      onChange={(e) => setPrice(e.target.value)}
-                      name="Section Name"
-                      value={price}
+                      onChange={(e) => {
+                        const updatedAccordions = accordions.map(
+                          (accordion, accIndex) => {
+                            if (index === accIndex) {
+                              return {
+                                ...accordion,
+                                unitPrice: e.target.value,
+                              };
+                            }
+                            return accordion;
+                          }
+                        );
+                        setAccordions(updatedAccordions);
+                      }}
+                      name="Price"
+                      value={accordions[index].unitPrice}
                     ></Form>
                     <Form
                       label="Capacity"
                       type="number"
                       required
-                      onChange={(e) => setCapacity(e.target.value)}
+                      onChange={(e) => {
+                        const updatedAccordions = accordions.map(
+                          (accordion, accIndex) => {
+                            if (index === accIndex) {
+                              return { ...accordion, capacity: e.target.value };
+                            }
+                            return accordion;
+                          }
+                        );
+                        setAccordions(updatedAccordions);
+                      }}
                       name="Capacity"
-                      value={capacity}
+                      value={accordions[index].capacity}
                     ></Form>
                   </AccordionDetails>
                 </Accordion>
               );
             })
           )}
-          <TextField
-            sx={{ marginTop: "2rem" }}
-            id="outlined-select-currency"
-            select
-            label="Invite Planner(option)"
-            defaultValue="EUR"
-            helperText="Please select other palnner"
+          <InputLabel id="demo-simple-select-filled-label">
+            Venue(Reuqired)
+          </InputLabel>
+          <Select
+            sx={{ width: "200px" }}
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            value={venue}
+            onChange={(e) => setVenue(e.target.value)}
           >
-            {currencies.map((option) => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
+            {venues.length === 0 ? (
+              <></>
+            ) : (
+              venues.map((venue) => (
+                <MenuItem
+                  key={venue.id}
+                  value={venue.name}
+                  onInput={(e) => console.log(e)}
+                >
+                  {venue.name}
+                </MenuItem>
+              ))
+            )}
+          </Select>
         </div>
       </div>
-      <Button type="submit" buttonType="google">
+      {/* <Button type="click" buttonType="google" onClick={() => createEvent()}>
         Create event
-      </Button>
+      </Button> */}
+      <button id="create-event-button" onClick={(e) => createEvent(e)}>
+        Create event
+      </button>
     </div>
     // </div>
   );
