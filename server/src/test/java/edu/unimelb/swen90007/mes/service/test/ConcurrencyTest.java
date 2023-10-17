088c932a1438e1b0590d8f4715f830189c88d600
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-10-17 11:21:58 +1100

    remove redundant Admin class

--- server/src/test/java/edu/unimelb/swen90007/mes/service/test/ConcurrencyTest.java
+++ server/src/test/java/edu/unimelb/swen90007/mes/service/test/ConcurrencyTest.java
@@ -1,11 +1,25 @@
 package edu.unimelb.swen90007.mes.service.test;
 
+import edu.unimelb.swen90007.mes.model.Venue;
+import edu.unimelb.swen90007.mes.service.impl.AdminService;
+
 public class ConcurrencyTest {
+    /**
+     * Simulate the administrator to create two venues.
+     */
+    public static void createVenue() {
+        AdminService adminService = new AdminService();
+        Venue venue1 = new Venue("Mock Venue 1", "Mock Venue 1 Address", 1000);
+        Venue venue2 = new Venue("Mock Venue 2", "Mock Venue 2 Address", 1000);
+        adminService.createVenue(venue1);
+        adminService.createVenue(venue2);
+    }
+
     public static void main(String[] args) {
         System.out.println("Concurrency Testing Started");
 
         // Simulate the administrator to create two venues.
-        Admin.createVenue();
+        createVenue();
 
         // Create three event planners.
         CreateEventThread ep1 = new CreateEventThread
@@ -28,8 +42,6 @@ public class ConcurrencyTest {
 
         ep3.inviteEventPlanner(ep4.getMockEventPlanner());
         ep4.inviteEventPlanner(ep3.getMockEventPlanner());
-
-        Admin.viewUsers();
 
         ep1.start();
         ep2.start();
