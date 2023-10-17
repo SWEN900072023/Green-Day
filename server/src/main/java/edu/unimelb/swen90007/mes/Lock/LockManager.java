4c8e000cf2dde6727650f2953f1b10847c9fad6c
Quanchi Chen <quanchic@student.unimelb.edu.au>
2023-10-17 14:00:48 +1100

    reformat LockManager

--- server/src/main/java/edu/unimelb/swen90007/mes/Lock/LockManager.java
+++ server/src/main/java/edu/unimelb/swen90007/mes/Lock/LockManager.java
@@ -7,9 +7,6 @@ import edu.unimelb.swen90007.mes.model.Section;
 import edu.unimelb.swen90007.mes.model.SubOrder;
 
 import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.locks.Lock;
@@ -19,24 +16,24 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LockManager {
     private static LockManager instance;
-    private final ConcurrentMap<Integer, ReadWriteLock> ticketsLocks;
     public final Lock validationLock = new ReentrantLock();
-
-    public static synchronized LockManager getInstance() {
-        if(instance == null) {
-            instance = new LockManager();
-        }
-        return instance;
-    }
+    private final ConcurrentMap<Integer, ReadWriteLock> ticketsLocks;
 
     private LockManager() {
         ticketsLocks = new ConcurrentHashMap<>();
     }
 
+    public static synchronized LockManager getInstance() {
+        if (instance == null) {
+            instance = new LockManager();
+        }
+        return instance;
+    }
+
     public void acquireTicketsReadLock(Event event) {
-        try{
+        try {
             event = EventMapper.loadByIdAll(event.getId());
-            for(Section section : event.getSections()){
+            for (Section section : event.getSections()) {
                 if (!ticketsLocks.containsKey(section.getId())) {
                     ticketsLocks.put(section.getId(), new ReentrantReadWriteLock());
                 }
@@ -49,14 +46,14 @@ public class LockManager {
     }
 
     public void releaseTicketsReadLock(Event event) {
-        for(Section section : event.getSections()){
+        for (Section section : event.getSections()) {
             Lock readLock = ticketsLocks.get(section.getId()).readLock();
             readLock.unlock();
         }
     }
 
     public void acquireTicketsWriteLock(Order order) {
-        for(SubOrder subOrder : order.getSubOrders()){
+        for (SubOrder subOrder : order.getSubOrders()) {
             int sectionId = subOrder.getSection().getId();
             if (!ticketsLocks.containsKey(sectionId)) {
                 ticketsLocks.put(sectionId, new ReentrantReadWriteLock());
@@ -67,7 +64,7 @@ public class LockManager {
     }
 
     public void releaseTicketsWriteLock(Order order) {
-        for(SubOrder subOrder : order.getSubOrders()){
+        for (SubOrder subOrder : order.getSubOrders()) {
             int sectionId = subOrder.getSection().getId();
             Lock writeLock = ticketsLocks.get(sectionId).writeLock();
             writeLock.unlock();
