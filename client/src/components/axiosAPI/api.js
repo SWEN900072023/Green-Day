8275d68aea528e1a1fe3362f079aad05987025da
StevenDB <xieyijie98@gmail.com>
2023-11-01 21:38:13 +1100

    fix[api.js]: use deployed url

--- client/src/components/axiosAPI/api.js
+++ client/src/components/axiosAPI/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8090/api",
+  baseURL: "https://mes-24tf.onrender.com/mes/api",
+  // baseURL: "http://localhost:8090/api",
 });
